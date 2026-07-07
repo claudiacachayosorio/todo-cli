5602fd9a38a37152c0a4ba206682e620f333d94c
claudiacachayosorio <claudiacachayosorio.web@gmail.com>
2026-07-07 18:54:01 -0400

    feat: main script skeleton

---
+++ todo.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# ================================================================================================= #
+# todo.sh: A simple command-line todo list manager													#
+# ================================================================================================= #
+# To execute this script, run: ./todo.sh															#
+
+
+
+# Global variables
+# ================================================================================================= #
+
+todotxt_path="./todo.txt"
+donetxt_path="./done.txt"
+
+
+
+# Functions
+# ================================================================================================= #
+
+# view all todo items
+view_todos() {}
+
+# view all done items
+view_done() {}
+
+# create a new todo item
+create_todo() {}
+
+# delete a todo item
+delete_todo() {}
+
+# reword a todo item
+reword_todo() {}
+
+# mark a todo item as done
+todo_to_done() {}
+
+# mark a done item as todo
+done_to_todo() {}
+
+# main menu for todo list manager
+main_menu() {}
+
+
+
+# Point of entry
+# ================================================================================================= #
+
+main_menu
