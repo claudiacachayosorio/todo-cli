7d0f36f4ec6182201c489d279d3f3b943319d76d
claudiacachayosorio <claudiacachayosorio.web@gmail.com>
2026-07-25 18:36:11 -0400

    feat: filter tasks

--- lib/subcommands/list.sh
+++ lib/subcommands/list.sh
@@ -8,17 +8,23 @@
 
 get_full_list() {
 	local path="$1"
-	local terms="$2"
-	cat -n "$path"
-}
-
-format_list() {
-	local raw="$1"
 	local space="^[[:space:]]*([0-9]+)[[:space:]]*"
 	local date="\[[0-9]{4}-[0-9]{2}-[0-9]{2}\][[:space:]]*"
+	cat -n "$path" | sed -E "s/${space}/\1 /g; s/${date}//g"
+}
 
+filter_output() {
+	local data="$1"
+	shift
+
+	local keyword
 	local output
-	output=$(sed -E "s/${space}/\1 /g; s/${date}//g" <<< "$raw")
+
+	for keyword in "$@"
+	do
+		output=$(echo "$data" | grep -iF "$keyword" || true)
+	done
+
 	echo "$output"
 }
 
@@ -52,18 +58,31 @@ print_list() {
 # EXECUTION FLOW ====================================================================== #
 
 SRC_PATH="$TODOTXT"
+
 if [[ $# -gt 0 ]]
 then
-	if [[ "$1" =~ ^[a-z]+:$ ]]
+	if [[ "$1" =~ ^[a-z]+.txt$ ]]
 	then
 		SRC_PATH=$(get_data_path "$1")
 		shift
 	fi
 fi
 
-SEARCH_TERMS="$@"
+if [[ ! -s "$SRC_PATH" ]]
+then
+	echo "'${SRC_PATH##*/}' is currently empty"
+	return 0
+fi
 
-RAW_CONTENT=$(get_full_list "$SRC_PATH" "$SEARCH_TERMS")
-LIST_CONTENT=$(format_list "$RAW_CONTENT")
+KEYWORDS="$@"
+INDEXED_TASKS=$(get_full_list "$SRC_PATH")
+
+if [[ $# -gt 0 ]]
+then
+	LIST_CONTENT=$(filter_output "$INDEXED_TASKS" "$KEYWORDS")
+else
+	LIST_CONTENT="$INDEXED_TASKS"
+fi
+
 LIST_FOOTER=$(get_footer "$SRC_PATH" "$LIST_CONTENT")
 print_list "$LIST_CONTENT" "$LIST_FOOTER"
