d9fbeb0d2dda5749e40aa2fbeca756c738367d26
claudiacachayosorio <claudiacachayosorio.web@gmail.com>
2026-08-01 22:12:58 -0400

    chore: moved all subcommand logic into app.sh

--- lib/app.sh
+++ lib/app.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# ===================================================================================== #
-# Description:	User interface functions for todo-cli.
-# ===================================================================================== #
+# =========================================================================== #
+# Description:	Subcommand functions for todo-cli.
+# =========================================================================== #
 
-# ===================================================================================== #
+# =========================================================================== #
 # Command:		bash todo.sh help
-# ===================================================================================== #
+# =========================================================================== #
 
 todo_help() {
 	cat <<- EOF
@@ -15,19 +15,87 @@ todo_help() {
 	bash todo.sh <command> [<argument>...]
 
 	COMMANDS
-	add           <task [+<project-tag>...] [@<context-tag>...] ...>
-	list, ls      [<filename>] [<keyword> [or <keyword> ...] ...]
-	remove, rm    <line-number>...
-	done          <line-number>...
-	undo          <line-number>...
+	add   <task [+<project-tag>...] [@<context-tag>...] ...>
+	del   <line-number>...
+	done  <line-number>...
+	undo  <line-number>...
+	list  [<filename>] [<keyword> [or <keyword> ...] ...]
 
 	EOF
 	exit 0
 }
 
-# ===================================================================================== #
-# Command:		bash todo.sh list | bash todo.sh ls
-# ===================================================================================== #
+# =========================================================================== #
+# Command:		bash todo.sh add
+# =========================================================================== #
+
+todo_add() {
+	echo "add: '${*}'"
+	#validate_arg_count "min" "1" "$#"
+	#local -r task_content="$*"
+	#local date_created
+	#local new_task
+
+	#date_created="$(date +%F)"
+	#new_task="${date_created} ${task_content}"
+	#printf "%s\n" "$new_task" >> "$TODO_ACTIVE_DATA"
+	#assert_task_exists "$new_task" "$TODO_ACTIVE_DATA"
+}
+
+# =========================================================================== #
+# Command:		bash todo.sh del
+# =========================================================================== #
+
+todo_del() {
+	echo "remove: '${*}'"
+	#validate_arg_count "min" "1" "$#"
+	#local total_deleted_lines="$#"
+	#local data_path="$TODO_ACTIVE_DATA"
+	#local line_count
+	#local line_number
+
+	#if [[ $1 =~ ^[a-z]+$ ]]; then
+	#	data_path="${TODO_DATA_DIR}/${1}.txt"
+	#	assert_file_exists "$data_path"
+	#	shift
+	#fi
+
+	#line_count="$(wc -l < $data_path)"
+
+	#for line_number in "$@"; do
+	#	if [[ ! "$line_number" =~ ^[0-9]+$ ]]; then
+	#		log_error "'${line_number}' is not an integer"
+	#		return 1
+
+	#	elif [[ "$line_number" -eq 0 || "$line_number" -gt "$line_count" ]]; then
+	#		log_error "line ${line_number} not found"
+	#		return 1
+
+	#	else
+	#		sed -i "${line_number}d" "$data_path"
+	#	fi
+	#done
+}
+
+# =========================================================================== #
+# Command:		bash todo.sh done
+# =========================================================================== #
+
+todo_done() {
+	echo "done: '${*}'"
+}
+
+# =========================================================================== #
+# Command:		bash todo.sh undo
+# =========================================================================== #
+
+todo_undo() {
+	echo "undo: '${*}'"
+}
+
+# =========================================================================== #
+# Command:		bash todo.sh list
+# =========================================================================== #
 
 get_all_tasks() {
 	local -r src="$1"
--- todo.sh
+++ todo.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# ===================================================================================== #
+# =========================================================================== #
 # Description:	todo-cli's main script.
 # Command:		bash todo.sh
-# ===================================================================================== #
+# =========================================================================== #
 
 set -euo pipefail
 shopt -s extglob
 
 readonly TODO_APP_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-readonly TODO_CONFIG="${TODO_APP_ROOT}/todo.cfg"
+readonly TODO_CONFIG="${TODO_APP_ROOT}/todo.conf"
 if [[ -f "$TODO_CONFIG" ]]; then
 	source "$TODO_CONFIG"
 fi
@@ -18,8 +18,7 @@ fi
 readonly TODO_LIB_DIR="${TODO_APP_ROOT}/lib"
 source "${TODO_LIB_DIR}/vars.sh"
 source "${TODO_LIB_DIR}/utils.sh"
-source "${TODO_LIB_DIR}/api.sh"
-source "${TODO_LIB_DIR}/ui.sh"
+source "${TODO_LIB_DIR}/app.sh"
 
 readonly TODO_DATA_DIR="${TODO_APP_ROOT}/data"
 readonly TODO_ACTIVE_DATA="${TODO_DATA_DIR}/todo.txt"
@@ -33,20 +32,19 @@ main() {
 		todo_help
 	fi
 
-	local -r command="${1:-}"
+	local -r command="$1"
 	shift
 
 	case "$command" in
-		help)		todo_help "$@" ;;
-		add|+)		todo_add "$@" ;;
-		list|ls)	todo_list "$@" ;;
-		remove|rm)	todo_remove "$@" ;;
-		done)		todo_done "$@" ;;
-		undo)		todo_undo "$@" ;;
+		add)	todo_add "$@" ;;
+		del)	todo_del "$@" ;;
+		done)	todo_done "$@" ;;
+		undo)	todo_undo "$@" ;;
+		list)	todo_list "$@" ;;
+		help)	todo_help "$@" ;;
 		*)
 			log_error "'${command}': Invalid command."
-			return 1
-			;;
+			return 1 ;;
 	esac
 }
 
